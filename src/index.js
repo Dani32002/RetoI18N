0b36620d09dae13e1d521b804192f17d9e4e6768
Daniel Escalante <d.escalante@uniandes.edu.co>
2024-03-12 10:17:11 -0500

    Reto Completado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { IntlProvider } from "react-intl";
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+const checkLanguage = () => {
+    console.log(navigator.language);
+    return navigator.language.startsWith("en");
+}
+
+ReactDOM.render(
+    <IntlProvider locale={(checkLanguage())? "en": "es"} messages= {(checkLanguage())? localeEnMessages: localeEsMessages}>
+        <JobsList />
+    </IntlProvider>, document.getElementById("root"));
